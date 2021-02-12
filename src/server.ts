61bb307f933a2b0281e4b213422a6fff4c2f1445
lsantos <lsantos.ls@outlook.com>
2021-02-12 00:40:54 -0300

    Ajustando o dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
+import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 
 const app = express();
+
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
